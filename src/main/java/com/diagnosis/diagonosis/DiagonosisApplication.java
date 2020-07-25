d6da6623480289829ad010ead593b1cab253c146
adeel <adeelahmedcsku@gmail.com>
2020-07-25 16:01:30 -0400

    added new projeect

---
+++ src/main/java/com/diagnosis/diagonosis/DiagonosisApplication.java
@@ -0,0 +1,13 @@
+package com.diagnosis.diagonosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagonosisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiagonosisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diagnosis/diagonosis/DiagonosisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diagnosis.diagonosis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagonosisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
